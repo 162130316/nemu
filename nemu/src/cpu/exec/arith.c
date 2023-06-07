2c834230f15a1c01189893f30a8266244d5ebe4a
tracer-ics2017 <tracer@njuics.org>
2023-06-07 10:19:46 +0800

    >  compile 162130316 jianghang Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  10:19:46 up 7 min,  1 user,  load average: 0.10, 0.03, 0.01 50119cdf42001f62f2f2ee5feaac740645cecc25

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -46,7 +46,12 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-  TODO();
+  rtl_subi(&t0, &id_dest->val, 1);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_xor(&t1, &id_dest->val, &t0);
+  rtl_msb(&t1, &t1, id_dest->width);
+  rtl_set_OF(&t1);
 
   print_asm_template1(inc);
 }
