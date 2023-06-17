5428a1fe582d3ee9056b29c498586a621f514fa4
tracer-ics2017 <tracer@njuics.org>
2023-06-17 16:07:05 +0800

    >  compile 162130316 jianghang Linux debian 4.19.0-8-686-pae #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  16:07:05 up  1:34,  1 user,  load average: 0.04, 0.01, 0.00 8b6fba6cb122bfe437da28882be61549a1fe872b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -48,6 +48,11 @@ typedef struct {
   //rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
+  struct {
+      uint32_t base;  
+      uint16_t limit; 
+  }idtr;
+  uint16_t cs;
 
 } CPU_state;
 
--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,15 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  vaddr_t gate_addr = cpu.idtr.base + 8 * NO;
+   if (cpu.idtr.limit < 0) assert(0);
+   rtl_push(&cpu.eflags.all);
+   rtl_push(&t0);
+   rtl_push(&ret_addr);
+   t1 = vaddr_read(gate_addr, 4) & 0xffff;
+   t2 = vaddr_read(gate_addr + 4, 4) & 0xffff0000;
+   decoding.jmp_eip = t1 | t2;
+   decoding.is_jmp = true;
 }
 
 void dev_raise_intr() {
